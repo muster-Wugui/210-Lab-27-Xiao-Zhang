ab806e672eb9b024f578221f9f6b5259041f64f3
muster-Wugui <tintinpost28@gmail.com>
2024-11-02 14:12:15 -0700

    Second Commit

--- 210-Lab 27-Xiao Zhang/main.cpp
+++ 210-Lab 27-Xiao Zhang/main.cpp
@@ -9,12 +9,12 @@ using namespace std;
 
 using vil = tuple<int, string, string>;
 
-void display (map<string, vil>& villagers);
 void addvil (map<string, vil>& villagers);
 void deletevil (map<string, vil>& villagers);
 void increase_friend (map<string, vil>& villagers);
 void decrease_friend (map<string, vil>& villagers);
-void searchvil (map<string, vil>& villagers);
+void searchvil (const map<string, vil>& villagers);
+void display (const map<string, vil>& villagers);
 
 int main(){
     map<string, vil> villagers;
@@ -53,3 +53,42 @@ int main(){
        } while (choice != 6);
     
 }
+
+void addvil (map<string, vil>& villagers){
+    string name;
+    string species;
+    string catchphrase;
+    int level = -1;
+    
+    cout << "Villager name: ";
+    cin >> name;
+    cout<<endl;
+    cout << "Friendship level: ";
+    cin >> level;
+    if (level < 0){
+        cout<<"Friendship Level can't be negative!";
+        return;
+    }
+    cout<<endl;
+    cout << "Species: ";
+    cin >> species;
+    cout<<endl;
+    cout << "Catchphrase: ";
+    cin>>catchphrase;
+    cin.ignore();
+    getline(cin, catchphrase);
+    
+    villagers[name] = make_tuple(level, species, catchphrase);
+    cout << name << " added.\n";
+}
+
+void deletevil (map<string, vil>& villagers) {
+    string name;
+    cout << "Which villager do you want to delete: ";
+    cin >> name;
+
+    villagers.erase(name);
+    cout << name << " is deleted.\n";
+}
+
+void increase_friend (map<string, vil>& villagers);
